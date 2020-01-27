ceb284bce56203f494a4db30a9632f833adfd3c7
VACARU CIPRIAN <ciprianv@VACARUs-MacBook-Pro.local>
2020-01-27 16:14:49 +0200

    add boilerplate for project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">test</div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
